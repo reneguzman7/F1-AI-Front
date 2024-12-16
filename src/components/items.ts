2cf686ae3ddb7c77f2fb68f3236ff474a6469b84
reneguzman7 <reneguzman7@outlook.com>
2024-12-16 14:45:48 -0500

    rama a seguir

--- src/components/items.ts
+++ src/components/items.ts
@@ -3,65 +3,122 @@
 export const items = [
     {
       number: 1,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_mclaren_logo.png', // Escudo de McLaren
+      pilotImage: '/assets/Drivers/lannor01.avif', // Lando Norris
+      carImage: '/assets/Cars/mclaren.avif', // Coche de McLaren
     },
     {
       number: 2,
-      shieldImage: '/assets/Teams/ferrari-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/estoco01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/ferrari.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_mclaren_logo.png', // Escudo de McLaren
+      pilotImage: '/assets/Drivers/oscpia01.avif', // Oscar Piastri
+      carImage: '/assets/Cars/mclaren.avif', // Coche de McLaren
     },
     {
       number: 3,
-      shieldImage: '/assets/Teams/mercedes-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/haas.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_ferrari_logo.png', // Escudo de Ferrari
+      pilotImage: '/assets/Drivers/carsai01.avif', // Carlos Sainz
+      carImage: '/assets/Cars/ferrari.avif', // Coche de Ferrari
     },
     {
       number: 4,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_redbull_logo.png', // Escudo de Red Bull Racing
+      pilotImage: '/assets/Drivers/maxver01.avif', // Max Verstappen
+      carImage: '/assets/Cars/red-bull-racing.avif', // Coche de Red Bull Racing
     },
     {
       number: 5,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_alpine_logo.png', // Escudo de Alpine
+      pilotImage: '/assets/Drivers/piegas01.avif', // Pierre Gasly
+      carImage: '/assets/Cars/alpine.avif', // Coche de Alpine
     },
     {
       number: 6,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_mercedes_logo.png', // Escudo de Mercedes
+      pilotImage: '/assets/Drivers/georus01.avif', // George Russell
+      carImage: '/assets/Cars/mercedes.avif', // Coche de Mercedes
     },
     {
       number: 7,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_haas_logo.png', // Escudo de Haas
+      pilotImage: '/assets/Drivers/nichul01.avif', // Nico Hulkenberg
+      carImage: '/assets/Cars/haas.avif', // Coche de Haas
     },
     {
       number: 8,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_astonmartin_logo.png', // Escudo de Aston Martin
+      pilotImage: '/assets/Drivers/feralo01.avif', // Fernando Alonso
+      carImage: '/assets/Cars/aston-martin.avif', // Coche de Aston Martin
     },
     {
       number: 9,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/stake-f1-team-logo-wit.png', // Escudo de Kick Sauber
+      pilotImage: '/assets/Drivers/valbot01.avif', // Valtteri Bottas
+      carImage: '/assets/Cars/kick-sauber.avif', // Coche de Kick Sauber
     },
     {
       number: 10,
-      shieldImage: '/assets/Teams/alpine-logo.avif', // URL de la imagen del escudo
-      pilotImage: '/assets/Drivers/alealb01.avif',  // URL de la imagen del piloto
-      carImage: '/assets/Cars/alpine.avif',       // URL de la imagen del carro
+      shieldImage: '/assets/Teams/f1_2021_redbull_logo.png', // Escudo de Red Bull Racing
+      pilotImage: '/assets/Drivers/serper01.avif', // Sergio Perez
+      carImage: '/assets/Cars/red-bull-racing.avif', // Coche de Red Bull Racing
     },
-
-    // Añade más items aquí si es necesario
+    {
+      number: 11,
+      shieldImage: '/assets/Teams/f1_2022_visacachapprb_logo.png', // Escudo de Visa Cash App
+      pilotImage: '/assets/Drivers/yuktsu01.avif', // Yuki Tsunoda
+      carImage: '/assets/Cars/rb.avif', // Coche de AlphaTauri (si no hay, se usa rb.avif)
+    },
+    {
+      number: 12,
+      shieldImage: '/assets/Teams/f1_2022_visacachapprb_logo.png', // Escudo de Visa Cash App
+      pilotImage: '/assets/Drivers/lialaw01.avif', // Liam Lawson
+      carImage: '/assets/Cars/rb.avif', // Coche de AlphaTauri (si no hay, se usa rb.avif)
+    },
+    {
+      number: 13,
+      shieldImage: '/assets/Teams/f1_2021_astonmartin_logo.png', // Escudo de Aston Martin
+      pilotImage: '/assets/Drivers/lanstr01.avif', // Lance Stroll
+      carImage: '/assets/Cars/aston-martin.avif', // Coche de Aston Martin
+    },
+    {
+      number: 14,
+      shieldImage: '/assets/Teams/f1_2021_haas_logo.png', // Escudo de Haas
+      pilotImage: '/assets/Drivers/kevmag01.avif', // Kevin Magnussen
+      carImage: '/assets/Cars/haas.avif', // Coche de Haas
+    },
+    {
+      number: 15,
+      shieldImage: '/assets/Teams/stake-f1-team-logo-wit.png', // Escudo de Kick Sauber
+      pilotImage: '/assets/Drivers/guazho01.avif', // Zhou Guanyu
+      carImage: '/assets/Cars/kick-sauber.avif', // Coche de Kick Sauber
+    },
+    {
+      number: 16,
+      shieldImage: '/assets/Teams/f1_2021_mercedes_logo.png', // Escudo de Mercedes
+      pilotImage: '/assets/Drivers/lewham01.avif', // Lewis Hamilton
+      carImage: '/assets/Cars/mercedes.avif', // Coche de Mercedes
+    },
+    {
+      number: 17,
+      shieldImage: '/assets/Teams/f1_2021_alpine_logo.png', // Escudo de Alpine
+      pilotImage: '/assets/Drivers/jacdoo01.avif', // Jack Doohan
+      carImage: '/assets/Cars/alpine.avif', // Coche de Alpine
+    },
+    {
+      number: 18,
+      shieldImage: '/assets/Teams/f1_2021_williams_logo.png', // Escudo de Williams
+      pilotImage: '/assets/Drivers/alealb01.avif', // Alexander Albon
+      carImage: '/assets/Cars/williams.avif', // Coche de Williams
+    },
+    {
+      number: 19,
+      shieldImage: '/assets/Teams/f1_2021_ferrari_logo.png', // Escudo de Ferrari
+      pilotImage: '/assets/Drivers/chalec01.avif', // Charles Leclerc
+      carImage: '/assets/Cars/ferrari.avif', // Coche de Ferrari
+    },
+    {
+      number: 20,
+      shieldImage: '/assets/Teams/f1_2021_williams_logo.png', // Escudo de Williams
+      pilotImage: '/assets/Drivers/fracol01.avif', // Franco Colapinto
+      carImage: '/assets/Cars/williams.avif', // Coche de Williams
+    },
   ];
-  
