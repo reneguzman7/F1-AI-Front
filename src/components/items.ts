30db5b7a3799335981ccb70fd6922e19c8f70d3a
reneguzman7 <reneguzman7@outlook.com>
2024-12-17 11:50:04 -0500

    starting grid set

--- src/components/items.ts
+++ src/components/items.ts
@@ -1,101 +1,103 @@
+// src/data/items.ts
+
 export const items = [
   {
     number: 1,
     shieldImage: '/assets/Teams/f1_2021_mclaren_logo.png', // Escudo de McLaren
     pilotImage: '/assets/Drivers/lannor01.avif', // Lando Norris
     carImage: '/assets/Cars/mclaren.avif', // Coche de McLaren
-    historicalData: "Lando Norris ha mostrado un rendimiento consistente con un promedio de clasificacion de 5.2. Su tasa de podios es baja, pero su promedio de posicion en circuito es destacable con 6.2.",
+    historicalData: "Lando Norris ha mostrado un rendimiento consistente con un promedio de clasificación de 5.2. Su tasa de podios es baja, pero su promedio de posición en circuito es destacable con 6.2.",
   },
   {
     number: 2,
     shieldImage: '/assets/Teams/f1_2021_mclaren_logo.png', // Escudo de McLaren
     pilotImage: '/assets/Drivers/oscpia01.avif', // Oscar Piastri
     carImage: '/assets/Cars/mclaren.avif', // Coche de McLaren
-    historicalData: "Oscar Piastri, con un promedio de clasificacion de 3, ha mostrado potencial, pero su tasa de podios es baja, con solo un 9% de podios.",
+    historicalData: "Oscar Piastri, con un promedio de clasificación de 3, ha mostrado potencial, pero su tasa de podios es baja, con solo un 9% de podios.",
   },
   {
     number: 3,
     shieldImage: '/assets/Teams/f1_2021_ferrari_logo.png', // Escudo de Ferrari
     pilotImage: '/assets/Drivers/carsai01.avif', // Carlos Sainz
     carImage: '/assets/Cars/ferrari.avif', // Coche de Ferrari
-    historicalData: "Carlos Sainz ha tenido una temporada solida, con un promedio de clasificacion de 10.44 y una tasa de podios del 9.7%. Su posicion en el circuito es consistentemente en torno a la 8� posicion.",
+    historicalData: "Carlos Sainz ha tenido una temporada sólida, con un promedio de clasificación de 10.44 y una tasa de podios del 9.7%. Su posición en el circuito es consistentemente en torno a la 8ª posición.",
   },
   {
     number: 4,
     shieldImage: '/assets/Teams/f1_2021_redbull_logo.png', // Escudo de Red Bull Racing
     pilotImage: '/assets/Drivers/maxver01.avif', // Max Verstappen
     carImage: '/assets/Cars/red-bull-racing.avif', // Coche de Red Bull Racing
-    historicalData: "Max Verstappen, el actual campeon, ha mantenido un rendimiento excepcional con un promedio de clasificacion de 4 y una tasa de podios del 53%. Su promedio de posicion en circuito es de 3.78.",
+    historicalData: "Max Verstappen, el actual campeón, ha mantenido un rendimiento excepcional con un promedio de clasificación de 4 y una tasa de podios del 53%. Su promedio de posición en circuito es de 3.78.",
   },
   {
     number: 5,
     shieldImage: '/assets/Teams/f1_2021_alpine_logo.png', // Escudo de Alpine
     pilotImage: '/assets/Drivers/piegas01.avif', // Pierre Gasly
     carImage: '/assets/Cars/alpine.avif', // Coche de Alpine
-    historicalData: "Pierre Gasly ha mostrado un desempenio moderado, con un promedio de clasificacion de 13.57. Su tasa de podios es baja, con solo un 3.1% de podios.",
+    historicalData: "Pierre Gasly ha mostrado un desempeño moderado, con un promedio de clasificación de 13.57. Su tasa de podios es baja, con solo un 3.1% de podios.",
   },
   {
     number: 6,
     shieldImage: '/assets/Teams/f1_2021_mercedes_logo.png', // Escudo de Mercedes
     pilotImage: '/assets/Drivers/georus01.avif', // George Russell
     carImage: '/assets/Cars/mercedes.avif', // Coche de Mercedes
-    historicalData: "George Russell ha tenido un rendimiento solido con un promedio de clasificacion de 12.8. Su tasa de podios es moderada, alcanzando el 10.6%.",
+    historicalData: "George Russell ha tenido un rendimiento sólido con un promedio de clasificación de 12.8. Su tasa de podios es moderada, alcanzando el 10.6%.",
   },
   {
     number: 7,
     shieldImage: '/assets/Teams/f1_2021_haas_logo.png', // Escudo de Haas
     pilotImage: '/assets/Drivers/nichul01.avif', // Nico Hulkenberg
     carImage: '/assets/Cars/haas.avif', // Coche de Haas
-    historicalData: "Nico Hulkenberg ha enfrentado desafios en el equipo Haas, con un promedio de clasificacion de 9.1 y sin podios hasta el momento.",
+    historicalData: "Nico Hulkenberg ha enfrentado desafíos en el equipo Haas, con un promedio de clasificación de 9.1 y sin podios hasta el momento.",
   },
   {
     number: 8,
     shieldImage: '/assets/Teams/f1_2021_astonmartin_logo.png', // Escudo de Aston Martin
     pilotImage: '/assets/Drivers/feralo01.avif', // Fernando Alonso
     carImage: '/assets/Cars/aston-martin.avif', // Coche de Aston Martin
-    historicalData: "Fernando Alonso, a pesar de estar en una etapa m�s avanzada de su carrera, sigue siendo competitivo, con una tasa de podios del 27.9% y un promedio de clasificacion de 9.92.",
+    historicalData: "Fernando Alonso, a pesar de estar en una etapa más avanzada de su carrera, sigue siendo competitivo, con una tasa de podios del 27.9% y un promedio de clasificación de 9.92.",
   },
   {
     number: 9,
     shieldImage: '/assets/Teams/stake-f1-team-logo-wit.png', // Escudo de Kick Sauber
     pilotImage: '/assets/Drivers/valbot01.avif', // Valtteri Bottas
     carImage: '/assets/Cars/kick-sauber.avif', // Coche de Kick Sauber
-    historicalData: "Valtteri Bottas ha mostrado un rendimiento solido con un promedio de clasificacion de 7.64 y una tasa de podios del 30%.",
+    historicalData: "Valtteri Bottas ha mostrado un rendimiento sólido con un promedio de clasificación de 7.64 y una tasa de podios del 30%.",
   },
   {
     number: 10,
     shieldImage: '/assets/Teams/f1_2021_redbull_logo.png', // Escudo de Red Bull Racing
     pilotImage: '/assets/Drivers/serper01.avif', // Sergio Perez
     carImage: '/assets/Cars/red-bull-racing.avif', // Coche de Red Bull Racing
-    historicalData: "Sergio Perez ha tenido una temporada consistente con un promedio de clasificacion de 8.92 y una tasa de podios de 13.5%.",
+    historicalData: "Sergio Perez ha tenido una temporada consistente con un promedio de clasificación de 8.92 y una tasa de podios de 13.5%.",
   },
   {
     number: 11,
     shieldImage: '/assets/Teams/f1_2022_visacachapprb_logo.png', // Escudo de Visa Cash App
     pilotImage: '/assets/Drivers/yuktsu01.avif', // Yuki Tsunoda
     carImage: '/assets/Cars/rb.avif', // Coche de AlphaTauri
-    historicalData: "Yuki Tsunoda ha tenido dificultades para destacarse, con un promedio de clasificacion de 8.67 y sin podios en su carrera.",
+    historicalData: "Yuki Tsunoda ha tenido dificultades para destacarse, con un promedio de clasificación de 8.67 y sin podios en su carrera.",
   },
   {
     number: 12,
     shieldImage: '/assets/Teams/f1_2022_visacachapprb_logo.png', // Escudo de Visa Cash App
     pilotImage: '/assets/Drivers/lialaw01.avif', // Liam Lawson
     carImage: '/assets/Cars/rb.avif', // Coche de AlphaTauri
-    historicalData: "Liam Lawson ha tenido pocas oportunidades en la F1 y aun no ha logrado puntos o podios.",
+    historicalData: "Liam Lawson ha tenido pocas oportunidades en la F1 y aún no ha logrado puntos o podios.",
   },
   {
     number: 13,
     shieldImage: '/assets/Teams/f1_2021_astonmartin_logo.png', // Escudo de Aston Martin
     pilotImage: '/assets/Drivers/lanstr01.avif', // Lance Stroll
     carImage: '/assets/Cars/aston-martin.avif', // Coche de Aston Martin
-    historicalData: "Lance Stroll ha tenido un rendimiento modesto con un promedio de clasificacion de 13.71 y una tasa de podios baja del 2.1%.",
+    historicalData: "Lance Stroll ha tenido un rendimiento modesto con un promedio de clasificación de 13.71 y una tasa de podios baja del 2.1%.",
   },
   {
     number: 14,
     shieldImage: '/assets/Teams/f1_2021_haas_logo.png', // Escudo de Haas
     pilotImage: '/assets/Drivers/kevmag01.avif', // Kevin Magnussen
     carImage: '/assets/Cars/haas.avif', // Coche de Haas
-    historicalData: "Kevin Magnussen ha mostrado un desempenio bajo con un promedio de clasificacion de 14.88 y una tasa de podios minima.",
+    historicalData: "Kevin Magnussen ha mostrado un desempeño bajo con un promedio de clasificación de 14.88 y una tasa de podios mínima.",
   },
   {
     number: 15,
@@ -109,34 +111,34 @@ export const items = [
     shieldImage: '/assets/Teams/f1_2021_mercedes_logo.png', // Escudo de Mercedes
     pilotImage: '/assets/Drivers/lewham01.avif', // Lewis Hamilton
     carImage: '/assets/Cars/mercedes.avif', // Coche de Mercedes
-    historicalData: "Lewis Hamilton ha mantenido su nivel de elite con un promedio de clasificacion de 2.67 y una tasa de podios impresionante de 59.3%.",
+    historicalData: "Lewis Hamilton ha mantenido su nivel de élite con un promedio de clasificación de 2.67 y una tasa de podios impresionante de 59.3%.",
   },
   {
     number: 17,
     shieldImage: '/assets/Teams/f1_2021_alpine_logo.png', // Escudo de Alpine
     pilotImage: '/assets/Drivers/jacdoo01.avif', // Jack Doohan
     carImage: '/assets/Cars/alpine.avif', // Coche de Alpine
-    historicalData: "Jack Doohan no ha tenido participacion activa en la F1 en este periodo.",
+    historicalData: "Jack Doohan no ha tenido participación activa en la F1 en este periodo.",
   },
   {
     number: 18,
     shieldImage: '/assets/Teams/f1_2021_williams_logo.png', // Escudo de Williams
     pilotImage: '/assets/Drivers/alealb01.avif', // Alexander Albon
     carImage: '/assets/Cars/williams.avif', // Coche de Williams
-    historicalData: "Alexander Albon ha mostrado un rendimiento promedio con un promedio de clasificacion de 11 y una tasa de podios del 3.3%.",
+    historicalData: "Alexander Albon ha mostrado un rendimiento promedio con un promedio de clasificación de 11 y una tasa de podios del 3.3%.",
   },
   {
     number: 19,
-    shieldImage: '/assets/Teams/f1_2021_williams_logo.png', // Escudo de Williams
-    pilotImage: '/assets/Drivers/logham01.avif', // Logan Sargeant
-    carImage: '/assets/Cars/williams.avif', // Coche de Williams
-    historicalData: "Logan Sargeant es un piloto en su primera temporada en F1, con un rendimiento en desarrollo. Su promedio de clasificacion es de 16.6, sin podios hasta la fecha.",
+    shieldImage: '/assets/Teams/f1_2021_ferrari_logo.png', // Escudo de Ferrari
+    pilotImage: '/assets/Drivers/chalec01.avif', // Charles Leclerc
+    carImage: '/assets/Cars/ferrari.avif', // Coche de Ferrari
+    historicalData: "Logan Sargeant es un piloto en su primera temporada en F1, con un rendimiento en desarrollo. Su promedio de clasificación es de 16.6, sin podios hasta la fecha.",
   },
   {
     number: 20,
-    shieldImage: '/assets/Teams/f1_2021_haas_logo.png', // Escudo de Haas
-    pilotImage: '/assets/Drivers/hargru01.avif', // Harrison Grime
-    carImage: '/assets/Cars/haas.avif', // Coche de Haas
-    historicalData: "Harrison Grime es un piloto novato que aun no ha competido en ninguna carrera de F1, por lo que no tiene datos de clasificacion ni de podios.",
-  },  
+    shieldImage: '/assets/Teams/f1_2021_williams_logo.png', // Escudo de Williams
+    pilotImage: '/assets/Drivers/fracol01.avif', // Franco Colapinto
+    carImage: '/assets/Cars/williams.avif', // Coche de Williams
+    historicalData: "Franco Colapinto es un piloto prometedor que reemplazo a media temporada a Logan Sargeant destacando en sus priemras carreras en la F1. Con un enfoque en su desarrollo y adaptación a los monoplazas más potentes, se espera que continúe ganando experiencia y mejorando sus resultados en el futuro.",
+  },
 ];
